6e191d235579eaf5f97eaffe35b8a262b38de870
vistel <vtchako99@gmail.com>
2021-10-24 21:49:27 +0200

    mise en place du header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+
 
 function App() {
   return (
+    <>
+      <Particles 
+      className="particules-canvas"
+        params={{
+          particles:{
+            number:{
+              value:100,
+              density:{
+                enable:true,
+                value_area:900
+              }
+            },
+            shape:{
+              type:"circle",
+              stroke:{
+                width: 6,
+                color: "#206baf"
+              }
+            }
+          }
+        }}
+      />
       <Navbar />
+      <Header />
+    </>
     );
 }
 
